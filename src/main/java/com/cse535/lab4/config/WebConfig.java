dc2376279c41e816245b53c80e75321b8eb08e6d
Rohit Bhalke <rohitbhalke@gmail.com>
2018-11-28 17:40:53 -0500

    Add CORS support

--- src/main/java/com/cse535/lab4/config/WebConfig.java
+++ src/main/java/com/cse535/lab4/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.cse535.lab4.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/index.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true).maxAge(3600);
     }
 }
